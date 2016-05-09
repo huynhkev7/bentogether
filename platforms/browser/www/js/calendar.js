3c14195da2dd36e6e1c4e398c501018bda9a8a1c
huynhkev7 <huynhkev@uw.edu>
2016-05-09 14:54:09 -0700

    changed UI of start screens

--- platforms/browser/www/js/calendar.js
+++ platforms/browser/www/js/calendar.js
@@ -1,39 +1,69 @@
 $(document).ready(function () {
-	  var queryString = new Array();
 
-        if (queryString.length == 0) {
-            if (window.location.search.split('?').length > 1) {
-                var params = window.location.search.split('?')[1].split('&');
-                for (var i = 0; i < params.length; i++) {
-                    var key = params[i].split('=')[0];
-                    var value = decodeURIComponent(params[i].split('=')[1]);
-                    queryString[key] = value;
-                }
+    var monthNames = ["January", "February", "March", "April", "May", "June",
+      "July", "August", "September", "October", "November", "December"
+    ]; 
+    var date = new Date();
+    //0-based months
+    var currentMonth = date.getMonth();
+
+    //get child name
+    var queryString = new Array();
+
+    if (queryString.length == 0) {
+        if (window.location.search.split('?').length > 1) {
+            var params = window.location.search.split('?')[1].split('&');
+            for (var i = 0; i < params.length; i++) {
+                var key = params[i].split('=')[0];
+                var value = decodeURIComponent(params[i].split('=')[1]);
+                queryString[key] = value;
             }
         }
-        console.log(queryString);
-        if (queryString["name"] != null) {
-           $("#calendar-title").text("Calendar for: " + queryString["name"]);
-        }
+    }
+    // if (queryString["name"] != null) {
+    //    $("#calendar-title").text("Calendar for: " + queryString["name"]);
+    // }
+  
 
+    buildCalendar(currentMonth);
 
+    //decrement current month
+    $("#leftControl").click(function(){
+        currentMonth--;
+        buildCalendar(currentMonth);
+    });
+
+    //increment current month
+    $("#rightControl").click(function(){
+        currentMonth++;
+        buildCalendar(currentMonth);
+    });
+
+    //get how many days in the month
     function daysInMonth(month,year) {
         return new Date(year, month, 0).getDate();
-    }    
+    }  
 
-    function buildCalendar(){
-        var date = new Date();
-        var currentMonth = date.getMonth();  
-        var numberOfDaysInMonth = daysInMonth(currentMonth - 1, 2016);
+    //create the calendar
+    function buildCalendar(currentMonth){
+        //get start of month, exact day
+        //get end of month, exact day
+        
+
+
+        var numberOfDaysInMonth = daysInMonth(currentMonth + 1, 2016);
         var numberOfWeeks = parseInt(numberOfDaysInMonth / 7);
         var currentDay = 1;
         var currentWeek = 1;
         var tableElement = $("#table");
+        $("#monthTitle").text(monthNames[currentMonth]);
+        //delete all week rows
+        $(".week").remove();
         //number of rows
         for(var i = 0; i < numberOfWeeks; i++){
             //for each week...
             currentWeek++;
-            var tableRowElement = $("<tr id='week_" + currentWeek + "' ></tr>");
+            var tableRowElement = $("<tr class='week' id='week_" + currentWeek + "' ></tr>");
             for(var j = 0; j < 7; j++){
                 var tableColumnElement = $("<th class='day'>" + currentDay + "</th>");
                 tableRowElement.append(tableColumnElement);
@@ -45,8 +75,9 @@ $(document).ready(function () {
         if(numberOfDaysInMonth - (currentDay - 1) > 0){
             console.log("entering");
             currentWeek++;
-            var tableRowElement = $("<tr id='week_" + currentWeek + "' ></tr>");
-            for(var i = 0; i < numberOfDaysInMonth - (currentDay - 1); i++){
+            var tableRowElement = $("<tr class='week' id='week_" + currentWeek + "' ></tr>");
+            var tempDays = currentDay;
+            for(var i = 0; i < numberOfDaysInMonth - (tempDays - 1); i++){
                 var tableColumnElement = $("<th class='day'>" + currentDay + "</th>");
                 tableRowElement.append(tableColumnElement);
                 currentDay++;                
@@ -57,11 +88,12 @@ $(document).ready(function () {
         $(".day").click(function(){
             if (confirm('Did you want to create a meal on this day?')) {
                 // Save it!
+                window.location = "build.html";
             } else {
                 // Do nothing!
             }                  
         });
     }
 
-    buildCalendar();
+
 });
